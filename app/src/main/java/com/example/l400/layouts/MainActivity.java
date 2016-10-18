3e6cdc2f466948a450ffe9e6ac979bceaff44736
haider222 <ilyashaider123@gmail.com>
2016-10-17 23:41:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/l400/layouts/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.l400.layouts;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView upload , downlaod , setting;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater ma = getMenuInflater();
+        ma.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return true;
+    }
+}
